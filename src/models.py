a75a87f1c171875f29c6e61a6497f05571d0a9f0
gleonag <gleonag@gmail.com>
2022-11-19 10:27:59 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -1,10 +1,11 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+import enum
 
 Base = declarative_base()
 
@@ -14,36 +15,47 @@ class User(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(50), nullable=True)
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
-
-class Favorites(Base):
-    __tablename__ = 'favoritos'
+class Post(Base):
+    __tablename__= 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    planet_id = Column(Integer, ForeignKey('planets.id'))
-    character_id = Column(Integer, ForeignKey('characters.id'))
+
+class Comment(Base):
+    __tablename__= 'comment'
+    id = Column(Integer, primary_key=True)  
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class MyEnum(enum.Enum):
+    image = 1
+    video = 2
+    story = 3
 
 
-class Planets(Base):
-    __tablename__ = 'planets'
-    id=Column(Integer, primary_key=True)
-    name = Column(String(50))
-    description = Column(String(250))
-    location = Column(String(250))
-    galaxy = Column(String(10))
-
-class Characters(Base):
-    __tablename__ = 'characters'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    home_planet=Column(String(50))
-    description=Column(String(250))
+class Media(Base):
+    __tablename__= 'media'
+    id = Column(Integer, primary_key=True)  
+    type = Column(Enum(MyEnum))
+    # declaramos una class con los type llamada MyEnum
+    url = Column(String(50))
+    post_id = Column(Integer,ForeignKey('post.id'))
 
 
 
-    def to_dict(self):
-        return {}
+
+    # def to_dict(self):
+    #     return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
